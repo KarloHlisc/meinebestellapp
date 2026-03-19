1a4162f82816e1206cf175510dbfd93443cfef17
Karlo Hlisc <karlohlisc.kh@gmail.com>
2026-03-19 19:26:25 +0100

    add function to the renderCategoryDishes() to render list menues

---
+++ db.js
@@ -0,0 +1,38 @@
+
+
+let dishes = [
+  {
+    category: "Pizza",
+    menues: [
+      {
+        name: "Margerita",
+        description: "Tomatensauce, Mozzarella, Oregano",
+        picture: "./assets/img/margerita_Pizza.jpg",
+        price: 6.75
+      },
+      {
+        name: "Salami",
+        description: "Tomatensauce, Oregano, Salami, Zwiebeln",
+        picture: "./assets/img/salami_Pizza.jpg",
+        price: 8.75
+      }
+    ]
+  },
+  {
+    category: "Burger",
+    menues: [
+      {
+        name: "Hamburger",
+        description: "Beschreibung Hamburger",
+        picture: "./assets/img/hamburger.jpg",
+        price: 6.75
+      },
+      {
+        name: "Cheeseburger",
+        description: "Beschreibung Cheeseburger",
+        picture: "./assets/img/cheeseburger.jpg",
+        price: 8.75
+      }
+    ]
+  }
+]
