fca7f702d4bb548876e0e4adca7a65597160fade
Karlo Hlisc <karlohlisc.kh@gmail.com>
2026-03-25 10:58:19 +0100

    changed variable from const into let

--- db.js
+++ db.js
@@ -1,5 +1,3 @@
-
-
 let dishes = [
   {
     category: "Pizza",
@@ -15,6 +13,18 @@ let dishes = [
         description: "Tomatensauce, Oregano, Salami, Zwiebeln",
         picture: "./assets/img/salami_Pizza.jpg",
         price: 8.75
+      },
+       {
+        name: "Napoli",
+        description: "mit Oliven, Sardellen und Kapern",
+        picture: "./assets/img/margerita_Pizza.jpg",
+        price: 14.50
+      },
+      {
+        name: "Capricciosa",
+        description: "mit Schinken, Egerlingen, Artischocken, Peperoni und Oliven",
+        picture: "./assets/img/salami_Pizza.jpg",
+        price: 14.90
       }
     ]
   },
@@ -29,9 +39,79 @@ let dishes = [
       },
       {
         name: "Cheeseburger",
-        description: "Beschreibung Cheeseburger",
+        description: "Beef, doppelt Cheddar, Salat, Tomate, Gurke, Burger Sauce, Ketchup im veganen Brioche Bun",
         picture: "./assets/img/cheeseburger.jpg",
-        price: 8.75
+        price: 19.75
+      },
+      {
+        name: "Classic Smash",
+        description: "Beef (well done), doppelt Cheddar, rote Zwiebel, Gurke, Ketchup, Sweet Honey Mustard im veganen Brioche Bun",
+        picture: "./assets/img/hamburger.jpg",
+        price: 11.75
+      },
+      {
+        name: "BBQ",
+        description: "Beef, Bacon, Röstzwiebel, Smoky BBQ Sauce, doppelt Cheddar, Salat, Tomate, Gurke im veganen Brioche Bun",
+        picture: "./assets/img/cheeseburger.jpg",
+        price: 14.90
+      }  
+    ]
+  },
+   {
+    category: "Salat",
+    menues: [
+      {
+        name: "Isalata Nizza",
+        description: "Verschiedene Rohkost, Blattsalat, Thunfisch, gekochtes Ei, Rote Zwiebeln, Oliven, Kapern, Balsamicodressing",
+        picture: "./assets/img/hamburger.jpg",
+        price: 14.90
+      },
+      {
+        name: "Insalata rucola i pomodorini",
+        description: "Rucola, Strauchtomaten, Grana Padana Parmigiano",
+        picture: "./assets/img/cheeseburger.jpg",
+        price: 6.90
+      },
+       {
+        name: "Ceasar Salad",
+        description: "Romanasalat, gehobelter Parmesan, geröstete Croutons, Cherrytomaten und Ceasar-Dressing",
+        picture: "./assets/img/hamburger.jpg",
+        price: 12.90
+      },
+      {
+        name: "Hirtenkäse Salat",
+        description: "mit Hirtenkäse, gemischtem Salat, Tomate, Karotten, Gurken und Mais",
+        picture: "./assets/img/cheeseburger.jpg",
+        price: 9.90
+      },
+    ]
+  },
+   {
+    category: "Getränke",
+    menues: [
+      {
+        name: "Coca-Cola 1,0l",
+        description: "Coca-Cola steht für einzigartigen Geschmack, Erfrischung und Momente voller Lebensfreude",
+        picture: "./assets/img/hamburger.jpg",
+        price: 3.65
+      },
+      {
+        name: "Sprite 0,5l",
+        description: "Bist du bereit für Sprite? Die einzigartige Formel aus grünen Limetten und sonnengelben Zitronen erfrischt dich maximal.",
+        picture: "./assets/img/cheeseburger.jpg",
+        price: 2.75
+      },
+       {
+        name: "Adelholzener ACE 0,5l",
+        description: "Mit Süßungsmittel(n)",
+        picture: "./assets/img/hamburger.jpg",
+        price: 2.75
+      },
+      {
+        name: "Mezzo Mix 0,5l",
+        description: "Mixt euch eine gute Zeit mit Mezzo Mix, dem erfrischend-leckeren Kuss aus Cola und Orange.",
+        picture: "./assets/img/cheeseburger.jpg",
+        price: 2.75
       }
     ]
   }
