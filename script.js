b6dbbda1ed87a11cdf2907116c64e651d11866c7
Karlo Hlisc <karlohlisc.kh@gmail.com>
2026-05-16 17:08:27 +0200

    fix naming classes from underscore to dash

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ function init() {
 }
 
 function renderCategoryDishes() {
-  let categoryDish = document.getElementById("main_menu");
+  let categoryDish = document.getElementById("main-menu");
   categoryDish.innerHTML = "";
+
+
 
   for (let index = 0; index < dishes.length; index++) {
     let dishName = dishes[index];
     categoryDish.innerHTML += `
-             <div class="category_name">
-            <img src="" alt="slika kategorije" class="category_image">
-            <h2 class="category_title">${dishName.category}</h2>
-        </div>
-        <div class="menu_list" id="menu_list${index}">
-         ${renderMenuList(index)}
+          <div class="category-section">
+              <div class="category-name">
+                <img src="${dishName.icon}" alt="category ${dishName.category}" class="category-image">
+                <h2 class="category-title">${dishName.category}</h2>
+              </div>
+          </div>
+        <div class="menu-list" id="menu-list${index}">
+          ${renderMenuList(index)}
         </div>
         `;
   }
@@ -28,17 +32,18 @@ function renderMenuList(index) {
   for (let dish = 0; dish < elementN.menues.length; dish++) {
     let thisDish = elementN.menues[dish];
     menuList += `
-                 <div class="menu_card">
-                <div class="card_description">
-                    <img src="" alt="slika jela" class="card_image">
-                    <div>
-                    <h3>${thisDish.name}</h3>
-                    <span class="item_description">${thisDish.description}</span>
-                    </div>
+                <div class="menu-card">
+                <div class="card-description-img">
+                    <img src="${thisDish.picture}" alt="${thisDish.name}" class="card-image">
+                    
                 </div>
-                <div class="card_price">
-                    <p class="item_price">${thisDish.price.toFixed(2).replace(".", ",")} €</p>
-                    <button class="add_item" id="add_item${index}" onclick="addToCart(${thisDish.id})">Add</button>
+                <div class="card-description-text">
+                    <h3>${thisDish.name}</h3>
+                    <p class="item_description">${thisDish.description}</p>
+                    </div>
+                <div class="card-price">
+                    <p class="item-price">${thisDish.price.toFixed(2).replace(".", ",")} €</p>
+                    <a class="add-item" id="add-item${index}" onclick="addToCart(${thisDish.id})">Add to basket</a>
                 </div>
             </div> 
         `;
@@ -91,7 +96,7 @@ function renderShoppingCartDesktop() {
 
   let amountNumber = document.getElementById("shoppingSum");
   amountNumber.innerHTML = "";
-  let total = 0;
+  let subtotal = 0;
 
   for (let i = 0; i < shoppingCart.length; i++) {
     const dish = shoppingCart[i];
@@ -101,16 +106,18 @@ function renderShoppingCartDesktop() {
     shoppingCartDesktop.innerHTML += `
                 <div>
                     <span id="dishAmount${i}">${dish.amount}x ${dish.name} - ${price.toFixed(2).replace(".", ",")} € </span>
-                  <div>
-                    <button onclick="removeFromCart(${dish.id})">➖</button>
-                    <button onclick="addToCart(${dish.id})">➕</button>   
+                  <div class="dish-amount-counter">
+                    <a onclick="removeFromCart(${dish.id})"><img src="./assets/icons/minus.svg"></a>
+                    ${dish.amount}<a onclick="addToCart(${dish.id})"><img src="./assets/icons/plus.svg"></a>   
                   </div>
                 </div>
-               `;
-
-    total += dish.price * dish.amount;
+                `;
+    subtotal += dish.price * dish.amount;
   }
-  amountNumber.innerHTML = `<div><span><b>Total: ${total.toFixed(2).replace(".", ",")} €</b></span></div>`;
+  let totalDelivery = subtotal + 4.99;
+  amountNumber.innerHTML = `<div><span><b>Subtotal: ${subtotal.toFixed(2).replace(".", ",")} €</b></span>
+  <br><span><b>Delivery fee: 4,99 €</b></span><br>
+  <span><b>Total: ${totalDelivery.toFixed(2).replace(".", ",")} €</b></span></div>`;
 }
 
 function renderShoppingCartMobile() {
