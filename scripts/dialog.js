d2961df6c4f43a93a44fc3ce92c57e013fc3520b
Karlo Hlisc <karlohlisc.kh@gmail.com>
2026-05-19 22:47:33 +0200

    dialog.js added and modifided

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,16 +1,18 @@
 const dialogRef = document.getElementById("myDialog");
+const hideShopCard = document.getElementById("section-shopingCards");
 
 function openDialog() {
+    hideShopCard.classList.add("d-none");
     dialogRef.showModal();
-deleteBasketItems();
-/*
+    shoppingCart = [];
+    deleteBasketItems();
+
     setTimeout(function () {
         closeDialog();
-    },
-        15000);
-        */
+        },5000);        
 }
 
 function closeDialog() {
+    hideShopCard.classList.remove("d-none");
     dialogRef.close();
 }
